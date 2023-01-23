6dd367058385f9b442367c8f74421b81a63e01de
maxnth <maximilian.noeth@protonmail.com>
2023-01-23 20:34:48 +0100

    disable usage of engine to allow merging of seperated workflow branches

--- src/components/Editor/WorkflowEditor.vue
+++ src/components/Editor/WorkflowEditor.vue
@@ -14,6 +14,7 @@ import SidebarDescriptionOption from '~/components/Editor/Custom/SidebarDescript
 
 // TODO: still uses options API as baklavajs v1.x is buggy with the composition API. Should be migrated when baklavajs v2.x is stable
 export default {
+  // eslint-disable-next-line vue/no-unused-components
   components: { HintOverlay, EditorMenubar },
   data() {
     return {
@@ -31,7 +32,7 @@ export default {
     this.editor.use(new OptionPlugin());
     // The engine plugin calculates the nodes in the graph in the
     // correct order using the "calculate" methods of the nodes
-    this.editor.use(this.engine);
+    // this.editor.use(this.engine);
 
     // Show a minimap in the top right corner
     this.viewPlugin.enableMinimap = true;
