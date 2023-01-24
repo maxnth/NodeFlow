8e231a50c0e4b39efeb760008c9e8e7ade1dd610
maxnth <maximilian.noeth@protonmail.com>
2023-01-24 16:19:02 +0100

    update sidebar description styling

--- src/components/Editor/Custom/SidebarDescriptionOption.vue
+++ src/components/Editor/Custom/SidebarDescriptionOption.vue
@@ -10,21 +10,22 @@ const props = defineProps({
     <h5
       class="mb-2 text-2xl font-bold tracking-tight text-white dark:text-white"
     >
-      Processor Description
+      Processor
     </h5>
-    <p class="font-normal text-white dark:text-white">
+    <p class="font-normal text-white dark:text-white mb-4 block max-w-sm p-2 border border-gray-200 rounded-lg shadow">
       {{ value['description'] }}
     </p>
     <h5 v-if='value["parameter_descriptions"]'
       class="mb-2 mt-2 text-2xl font-bold tracking-tight text-white dark:text-white"
     >
-      Parameter Descriptions
+      Parameter
     </h5>
     <div
       v-for="(value, key, index) in value['parameter_descriptions']"
+      class="mb-4 block max-w-sm p-2 border border-gray-200 rounded-lg shadow"
       :key=index>
       <h5
-        class="mt-2 mb-2 font-bold tracking-tight text-white dark:text-white"
+        class="underline font-bold tracking-tight text-white dark:text-white"
       >
         {{ key }}
       </h5>
