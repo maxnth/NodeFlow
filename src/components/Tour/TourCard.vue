4c8232180b77756a55c69adb7f88a38c7279eb40
maxnth <maximilian.noeth@protonmail.com>
2023-01-24 16:42:44 +0100

    add WIP notice to Tour card

--- src/components/Tour/TourCard.vue
+++ src/components/Tour/TourCard.vue
@@ -9,7 +9,7 @@ const emit = defineEmits(['activate-tour']);
     <h5
       class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
     >
-      Tour the NodeFlow editor
+      Tour the NodeFlow editor (WORK IN PROGRESS!)
     </h5>
     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
       Interactively explore the NodeFlow editor and get to know all major
