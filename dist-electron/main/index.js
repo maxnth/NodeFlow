3837bfd099140c5f000156063b3c347cb5614583
maxnth <maximilian.noeth@protonmail.com>
2023-04-18 16:52:33 +0200

    chore: update deps

--- dist-electron/main/index.js
+++ dist-electron/main/index.js
@@ -1,1 +1,94 @@
-"use strict";const n=require("electron"),p=require("node:os"),s=require("node:path");process.env.DIST_ELECTRON=s.join(__dirname,"..");process.env.DIST=s.join(process.env.DIST_ELECTRON,"../dist");process.env.PUBLIC=process.env.VITE_DEV_SERVER_URL?s.join(process.env.DIST_ELECTRON,"../public"):process.env.DIST;p.release().startsWith("6.1")&&n.app.disableHardwareAcceleration();process.platform==="win32"&&n.app.setAppUserModelId(n.app.getName());n.app.requestSingleInstanceLock()||(n.app.quit(),process.exit(0));let e=null;const r=s.join(__dirname,"../preload/index.js"),a=process.env.VITE_DEV_SERVER_URL,c=s.join(process.env.DIST,"index.html");async function l(){e=new n.BrowserWindow({title:"Hephaistos",icon:s.join(process.env.PUBLIC,"favicon.ico"),width:1200,height:750,webPreferences:{preload:r,nodeIntegration:!0,contextIsolation:!1}}),e.setMenu(null),e.setOpacity(1),process.env.VITE_DEV_SERVER_URL?(e.loadURL(a),e.webContents.openDevTools()):e.loadFile(c),e.webContents.on("did-finish-load",()=>{e==null||e.webContents.send("main-process-message",new Date().toLocaleString())}),e.webContents.setWindowOpenHandler(({url:o})=>(o.startsWith("https:")&&n.shell.openExternal(o),{action:"deny"}))}n.app.whenReady().then(l);n.app.on("window-all-closed",()=>{e=null,process.platform!=="darwin"&&n.app.quit()});n.app.on("second-instance",()=>{e&&(e.isMinimized()&&e.restore(),e.focus())});n.app.on("activate",()=>{const o=n.BrowserWindow.getAllWindows();o.length?o[0].focus():l()});n.ipcMain.handle("open-win",(o,t)=>{const i=new n.BrowserWindow({webPreferences:{preload:r,nodeIntegration:!0,contextIsolation:!1}});process.env.VITE_DEV_SERVER_URL?i.loadURL(`${a}#${t}`):i.loadFile(c,{hash:t})});
+"use strict";
+const electron = require("electron");
+const node_os = require("node:os");
+const node_path = require("node:path");
+const fs = require("fs");
+process.env.DIST_ELECTRON = node_path.join(__dirname, "..");
+process.env.DIST = node_path.join(process.env.DIST_ELECTRON, "../dist");
+process.env.PUBLIC = process.env.VITE_DEV_SERVER_URL ? node_path.join(process.env.DIST_ELECTRON, "../public") : process.env.DIST;
+if (node_os.release().startsWith("6.1"))
+  electron.app.disableHardwareAcceleration();
+if (process.platform === "win32")
+  electron.app.setAppUserModelId(electron.app.getName());
+if (!electron.app.requestSingleInstanceLock()) {
+  electron.app.quit();
+  process.exit(0);
+}
+let win = null;
+const preload = node_path.join(__dirname, "../preload/index.js");
+const url = process.env.VITE_DEV_SERVER_URL;
+const indexHtml = node_path.join(process.env.DIST, "index.html");
+async function createWindow() {
+  win = new electron.BrowserWindow({
+    title: "NodeFlow",
+    icon: node_path.join(process.env.PUBLIC, "favicon.ico"),
+    width: 1200,
+    height: 750,
+    webPreferences: {
+      preload,
+      // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
+      // Consider using contextBridge.exposeInMainWorld
+      // Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+  win.setMenu(null);
+  win.setOpacity(1);
+  if (process.env.VITE_DEV_SERVER_URL) {
+    win.loadURL(url);
+    win.webContents.openDevTools();
+  } else {
+    win.loadFile(indexHtml);
+  }
+  win.webContents.on("did-finish-load", () => {
+    win == null ? void 0 : win.webContents.send("main-process-message", (/* @__PURE__ */ new Date()).toLocaleString());
+  });
+  win.webContents.setWindowOpenHandler(({ url: url2 }) => {
+    if (url2.startsWith("https:"))
+      electron.shell.openExternal(url2);
+    return { action: "deny" };
+  });
+}
+electron.app.whenReady().then(createWindow);
+electron.app.on("window-all-closed", () => {
+  win = null;
+  if (process.platform !== "darwin")
+    electron.app.quit();
+});
+electron.app.on("second-instance", () => {
+  if (win) {
+    if (win.isMinimized())
+      win.restore();
+    win.focus();
+  }
+});
+electron.app.on("activate", () => {
+  const allWindows = electron.BrowserWindow.getAllWindows();
+  if (allWindows.length) {
+    allWindows[0].focus();
+  } else {
+    createWindow();
+  }
+});
+electron.ipcMain.on("save-file", async (event, data) => {
+  const homeDir = require("os").homedir();
+  const desktopDir = `${homeDir}/Desktop`;
+  const outName = `${desktopDir}/workflow_${Math.floor(Date.now() / 1e3)}.json`;
+  fs.writeFileSync(outName, data);
+});
+electron.ipcMain.handle("open-win", (_, arg) => {
+  const childWindow = new electron.BrowserWindow({
+    webPreferences: {
+      preload,
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+  if (process.env.VITE_DEV_SERVER_URL) {
+    childWindow.loadURL(`${url}#${arg}`);
+  } else {
+    childWindow.loadFile(indexHtml, { hash: arg });
+  }
+});
+//# sourceMappingURL=index.js.map
