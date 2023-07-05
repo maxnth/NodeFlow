29546b558b11e664dbb4d853f81fc6ce0ed67a00
maxnth <maximilian.noeth@protonmail.com>
2023-07-05 16:18:47 +0200

    fix(style): set correct dark bg color for app

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import Toast from 'primevue/toast';
 </script>
 
 <template>
-  <div id="app" class="flex min-h-screen font-sans">
+  <div id="app" class="flex min-h-screen font-sans dark:bg-gray-700">
     <Toast />
     <Sidebar />
     <div style="height: 100vh; width: 100%" class="dark:bg-gray-700">
