5b5660360b623510d0b1b7aac346a1be7017bbf5
maxnth <maximilian.noeth@protonmail.com>
2024-06-13 13:21:52 +0200

    add files

---
+++ src/components/Editor/Custom/viewNode.ts
@@ -0,0 +1,15 @@
+// @ts-nocheck
+import { AbstractNode, INodeState } from "@baklavajs/core";
+
+export interface IViewNodeState extends INodeState<unknown, unknown> {
+  position: { x: number; y: number };
+  width: number;
+  twoColumn: boolean;
+}
+
+export function setViewNodeProperties(node: AbstractNode) {
+  node.position = node.position ?? { x: 0, y: 0 };
+  node.disablePointerEvents = false;
+  node.twoColumn = node.twoColumn ?? false;
+  node.width = node.width ?? 200;
+}
